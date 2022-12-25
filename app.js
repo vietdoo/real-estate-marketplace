15fe89177310205b03d5dc3b3fb810f4be7647c3
vietdoo <dodoviet@icloud.com>
2022-12-25 22:57:04 +0700

    Hook React

---
+++ app.js
@@ -0,0 +1,124 @@
+import React, {useState} from 'react';
+import {render} from 'react-dom';
+import {StaticMap} from 'react-map-gl';
+import DeckGL from '@deck.gl/react';
+import {MapView} from '@deck.gl/core';
+import {IconLayer} from '@deck.gl/layers';
+
+import IconClusterLayer from './icon-cluster-layer';
+
+
+const DATA_URL = 'house_online.json';
+
+const MAP_VIEW = new MapView({repeat: true});
+const INITIAL_VIEW_STATE = {
+  longitude: 106.69111,
+  latitude: 10.76733,
+  zoom: 12,
+  maxZoom: 20,
+  pitch: 0,
+  bearing: 0
+};
+
+const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json';
+
+function renderTooltip(info) {
+  const {object, x, y} = info;
+  
+  
+
+  if (info.objects) {
+    return (
+      <div className="tooltip interactive" style={{left: x, top: y}}>
+        {info.objects.map(({name, year, mass, class: meteorClass}) => {
+          return (
+            <div key={name}>
+              <h5>{name}</h5>
+              <div>Year: {year || 'unknown'}</div>
+              <div>Class: {meteorClass}</div>
+              <div>Mass: {mass}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  if (!object) {
+    return null;
+  }
+
+  return object.cluster ? (
+    <div className="tooltip" style={{left: x, top: y}}>
+      {object.point_count} records
+    </div>
+  ) : (
+    <div className="tooltip" style={{left: x, top: y}}>
+        {object.name} 
+        <br></br>
+        Giá: {object.mass}
+    </div>
+  );
+}
+
+
+export default function App({
+  data = DATA_URL,
+  iconMapping = 'data/location-icon-mapping.json',
+  iconAtlas = 'data/location-icon-atlas.png',
+  showCluster = true,
+  mapStyle = MAP_STYLE
+}) {
+  
+  const [hoverInfo, setHoverInfo] = useState({});
+
+  const hideTooltip = () => {
+    setHoverInfo({});
+  };
+  const expandTooltip = info => {
+    if (info.picked && showCluster) {
+      setHoverInfo(info);
+    } else {
+      setHoverInfo({});
+    }
+  };
+
+  const layerProps = {
+    data,
+    pickable: true,
+    getPosition: d => d.coordinates,
+    iconAtlas,
+    iconMapping,
+    onHover: !hoverInfo.objects && setHoverInfo
+  };
+
+  const layer = showCluster
+    ? new IconClusterLayer({...layerProps, id: 'icon-cluster', sizeScale: 40})
+    : new IconLayer({
+        ...layerProps,
+        id: 'icon',
+        getIcon: d => 'marker',
+        sizeUnits: 'meters',
+        sizeScale: 2000,
+        sizeMinPixels: 6
+      });
+
+  return (
+    <DeckGL
+      layers={[layer]}
+      views={MAP_VIEW}
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={{dragRotate: false}}
+      onViewStateChange={hideTooltip}
+      onClick={expandTooltip}
+    >
+      <StaticMap reuseMaps mapStyle={mapStyle} preventStyleDiffing={true} />
+
+      {renderTooltip(hoverInfo)}
+    </DeckGL>
+  );
+}
+
+export function renderToDOM(container) {
+  render(<App />, container);
+}
