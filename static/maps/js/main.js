38195e32f3cbac193eb9178554159cafdca6fab6
TrinhExtremelyHandsome <82462470+dangtrinhhh@users.noreply.github.com>
2023-01-06 01:08:03 +0700

    new design

--- static/maps/js/main.js
+++ static/maps/js/main.js
@@ -12,10 +12,32 @@ function makePopup(house) {
     var price = house['price'];
     var url = house['url']
     var img = house['img'];
-    var imgStr = '<img src = "' + img +  '" style="width: 120px;" ></img>'
+    var imgStr = '<img src = "' + img +  '" style="width: 100px; height: 100px; object-fit: cover;" ></img>'
     var imgLink = '<a href = "' + url + '">Chi tiết</a>'
 
-    return title + '<h2>' + price + '</h2>' + imgStr + imgLink
+    return '<div class="row"><div class="column1" style="">' + imgStr + '</div><div class="column2" style="">' + title + '<h2>' + price + '</h2>' + imgLink + '</div></div>'
+}
+
+function makeAdvertise(house) {
+    var title = house['title'];
+    var price = house['price'];
+    var url = house['url']
+    var img = house['img'];
+    var imgStr = '<img src = "' + img +  '" style="width: 100%; height: 150px; object-fit: cover; border-radius: 5px;" ></img>'
+    var imgLink = '<a href = "' + url + '">Chi tiết</a>'
+
+    return imgStr + '<div style="margin: 10px;">' + title + '<h4>' + price + '</h4>' + imgLink + '</div>'
+}
+
+function getRandomArrayElements(arr, count) {
+    var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
 }
 
 async function getJSON(dist) {
@@ -39,6 +61,16 @@ async function caller() {
         marker.bindPopup(popup);
         markers.addLayer(marker);
     }
+
+    var agents = getRandomArrayElements(houses, 10);
+    var inner = "";
+    for(var i = 0; i < 10; i++) {
+        var agentInner = '<div class="house" style="width: 47%; height: 250px; float: left; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px; border-radius: 5px;">' + makeAdvertise(agents[i]) + '</div>';
+        inner += agentInner;
+    }
+
+    document.getElementById("house-container").innerHTML = inner;
+
     console.log("Done in ", performance.now() - startTime, " ms")
 }
 
@@ -49,15 +81,22 @@ async function resetLayer() {
 
     var startTime = performance.now()
 
-    var radios = document.getElementsByName('district');
+    var select = document.getElementById('district');
     var option = '';
-    for (var i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            option = radios[i].value;
-        }
-    }
+    option = select.value;
     
-    const houses = await this.getJSON(option);  
+    const houses = await this.getJSON(option); 
+    
+    var agents = getRandomArrayElements(houses, 10);
+    var inner = "";
+
+    for(var i = 0; i < 10; i++) {
+        var agentInner = '<div class="house" style="width: 47%; height: 250px; float: left; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px; border-radius: 5px;">' + makeAdvertise(agents[i]) + '</div>';
+        inner += agentInner;
+    }
+
+    document.getElementById("house-container").innerHTML = inner;
+
     console.log("Successfully request: ", houses.length, " houses in ", houses[0]['dist']);
     for (var i = 0; i < houses.length; i++) {
         var lat = houses[i]['lat'];
@@ -73,3 +112,4 @@ async function resetLayer() {
 }
 
 map.addLayer(markers);
+
